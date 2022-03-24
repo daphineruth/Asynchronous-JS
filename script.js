b26a42ea95cc99131cdeaf3497f87fb915b9573c
sheiswiz <dapneruth304@gmail>
2022-03-24 12:51:32 +0300

    fixed some bugs

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const getCountryData = function(country){
  const request = new XMLHttpRequest();
 
- request.open('GET', 'https://restcountries.com/v3.1/name/{kenya}');
+ request.open('GET', 'https://restcountries.com/v3.1/name/${country}');
  request.send();
 
 request.addEventListener('load', function(){
@@ -18,19 +19,22 @@ request.addEventListener('load', function(){
 
     const html = `
     <article class="country">
-    <img class="country__img" src="${data.flag}" />
+    <img class="country__img" src="${data.flags}" />
     <div class="country__data">
       <h3 class="country__name">"${data.name}"</h3>
       <h4 class="country__region">"${data.region}"</h4>
       <p class="country__row"><span>👫</span>"${(+data.population/1000000).toFixed(1)}"People</p>
       <p class="country__row"><span>🗣️</span>"${data.languages.eng}"</p>
-     
+      <p class="country__row"><span>💰</span>${data.currencies.kes}</p>
     </div>
   </article>`;
   countriesContainer.insertAdjacentHTML('beforeend', html);
+
   countriesContainer.style.opacity = 1;
 
 
 });
+};
+getCountryData('kenya');
 
 
