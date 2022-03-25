07259cfd6e4cb97b8daf02ef63c373546fa91faf
sheiswiz <daphneruth304@gmail.com>
2022-03-25 21:45:21 +0300

    consuming promises

--- script.js
+++ script.js
@@ -81,6 +81,18 @@ setTimeout(() => {
     }, 1000);
   }, 1000);
 }, 1000);
+ //consuming promises
+const getCountryData = function (country) {
+   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+      .then(function (response) {
+        console.log(response);
+         return response.json();
+       })
+       .then(function (data) {
+         console.log(data);
+        renderCountry(data[0]);
+       });
+      };
 
 
 
