f323cb7957dda9df6fd64ca7a508d1bb6b03f64f
sheiswiz <daphneruth304@gmail.com>
2022-03-27 11:32:02 +0300

    fixed bugs'

--- script.js
+++ script.js
@@ -3,6 +3,24 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+
+const renderCountry = function (data, className = '') {
+  const html = `<article class="country">
+  <img class="country__img" src="${data.flags}" />
+  <div class="country__data">
+    <h3 class="country__name">"${data.name}"</h3>
+    <h4 class="country__region">"${data.region}"</h4>
+    <p class="country__row"><span>👫</span>"${(+data.population/1000000).toFixed(1)}"People</p>
+    <p class="country__row"><span>🗣️</span>"${data.languages.eng}"</p>
+    <p class="country__row"><span>💰</span>${data.currencies.kes}</p>
+  </div>
+  
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
 /*
 const getCountryData = function(country){
  const request = new XMLHttpRequest();
