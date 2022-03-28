1e186c96af4929e76f989edd76bfb789196c2472
sheiswiz <daphneruth304@gmail.com>
2022-03-28 19:17:26 +0300

    handling execptions

--- script.js
+++ script.js
@@ -209,7 +209,7 @@ setTimeout(() => {
             });
           }
           
-
+*/
           const getPosition = function () {
             return new Promise(function (resolve, reject) {
               navigator.geolocation.getCurrentPosition(resolve, reject);
@@ -217,21 +217,33 @@ setTimeout(() => {
           };
 
           const whereAmI = async function () {
-          
+            try {
+              
               const pos = await getPosition();
               const { latitude: lat, longitude: lng } = pos.coords;
           
+            
               const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
-              
+              if (!resGeo.ok) throw new Error('Problem getting location data');
               const dataGeo = await resGeo.json();
-              console.log(dataGeo);
-          }
           
-              
-            
-            
-        
-
-
-*/
-        
+              // Country data
+              const res = await fetch(
+                `https://restcountries.eu/rest/v2/name/${dataGeo.country}`
+              );
+              if (!resGeo.ok) throw new Error('Problem getting country');
+              const data = await res.json();
+              renderCountry(data[0]);
+          
+              return `You are in ${dataGeo.city}, ${dataGeo.country}`;
+            } catch (err) {
+              console.error(`${err} `);
+              renderError(`💥${err.message}`);
+          
+              // Reject promise returned from async function
+              throw err;
+            }
+          };
+          
+          
+          
