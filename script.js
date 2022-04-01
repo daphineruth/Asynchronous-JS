34c00b22bd7bc91d69bc9088af4d0985f5acf36a
sheiswiz <daphneruth304@gmail.com>
2022-04-01 12:01:17 +0300

    reviewed promises in parallel

--- script.js
+++ script.js
@@ -292,12 +292,13 @@ setTimeout(() => {
 
 
           //Running promises in parallel
-
+          
           const get3Countries = async function (c1, c2, c3) {
             try {
-               const [data1] = await getJSON(
+               /* const [data1] = await getJSON(
                 `https://restcountries.com/v3.1/name/${c1}`
                );
+
               const [data2] = await getJSON(
                 `https://restcountries.com/v3.1/name/${c2}`
                );
@@ -305,6 +306,7 @@ setTimeout(() => {
                const [data3] = await getJSON(
                 `https://restcountries.com/v3.1/name/${c3}`
                 );
+                */
 
                 const data = await Promise.all([
                   getJSON(`https://restcountries.com/v3.1name/${c1}`),
